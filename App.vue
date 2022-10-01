b0951bc7ca27fde2bc8bb56b7bffa3b5c4bf3307
Hugo_Furtado <hugofurtadocof@gmail.com>
2022-09-30 22:04:40 -0300

    :sparkles: first commit

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <HeaderIndexVue />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderIndexVue from "@/components/HeaderIndex.vue";
+export default defineComponent({
+  components: { HeaderIndexVue },
+});
+</script>
+
+<style scoped></style>
---
+++ components/HeaderIndex.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="header">
+    <div id="person">
+      <img src="../assets/person.png" />
+      <div id="welcome">
+        <h1>
+          <span> Hello {{ username }} !</span>
+        </h1>
+        <h3>Welcome</h3>
+        <p>to my vue3 syntax test</p>
+      </div>
+    </div>
+    <div id="inputName">
+      <input v-model="username" placeholder=">" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div#header {
+  display: flex;
+  text-align: center;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  background-color: #053cb11a;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  color: #000000;
+}
+</style>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      username: "",
+    };
+  },
+};
+</script>
